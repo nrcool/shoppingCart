4e18971b3cd6ce3919e82ca61a9cb0017f8403db
NAQVI <nomanalam7214@gmail.com>
2019-09-27 16:04:38 +0200

    Updates

---
+++ precache-manifest.43123e19fd359d4ecc9a1bb5fb7c14cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e277b6b2ef948d655837cb007a610caa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "817a7136c641d5f6b8e6",
+    "url": "./static/css/main.a37a1195.chunk.css"
+  },
+  {
+    "revision": "7b2e52822fb26382a48a",
+    "url": "./static/js/2.b8699575.chunk.js"
+  },
+  {
+    "revision": "817a7136c641d5f6b8e6",
+    "url": "./static/js/main.bef32456.chunk.js"
+  },
+  {
+    "revision": "91dcb35fd23fdc479fed",
+    "url": "./static/js/runtime~main.7e894ccd.js"
+  },
+  {
+    "revision": "1f86dd2bbd6375713ce89ecc41823e4b",
+    "url": "./static/media/online-shopping.1f86dd2b.jpg"
+  }
+]);
